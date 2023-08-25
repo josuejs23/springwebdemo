aea7eefee3ccb7207a527d2c6abc7a0f9cfed45d
Josue Zorrilla <josuezorrilla2302@gmail.com>
2023-08-25 15:44:49 -0400

    Added a method to find student by id

--- src/main/java/com/josuejs/springwebdemo/rest/StudentRestController.java
+++ src/main/java/com/josuejs/springwebdemo/rest/StudentRestController.java
@@ -1,16 +1,39 @@
 package com.josuejs.springwebdemo.rest;
 
 import com.josuejs.springwebdemo.entity.Student;
-import org.springframework.web.bind.annotation.GetMapping;
+import jakarta.annotation.PostConstruct;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
+@RequestMapping("/api")
 public class StudentRestController {
+
+    private List<Student> studentList;
+
+    @PostConstruct
+    public void loadData(){
+        this.studentList = List.of( new Student(1, "Josue", "Zorrilla"),
+                new Student(2, "Lili", "Zorrilla"));
+    }
+
+    public List<Student> getStudentList() {
+        return studentList;
+    }
+
+    public void setStudentList(List<Student> studentList) {
+        this.studentList = studentList;
+    }
 
     @GetMapping("/students")
     public List<Student> getStudents(){
-        return  List.of( new Student(1, "Josue", "Zorrilla"),
-                new Student(2, "Lili", "Zorrilla")
-        );
+        return  studentList;
+    }
+
+    @GetMapping("/student/{id}")
+    public Student getStudentById(@PathVariable int id){
+        return studentList.stream()
+                .filter( student1 -> student1.getId() == id).findFirst().orElse(new Student());
     }
 }
